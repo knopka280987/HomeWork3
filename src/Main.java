be3037d72b99269e156320869fa5fd3489142a2d
Татьяна <fokT.M@yandex.ru>
2022-08-08 01:19:50 +0300

    Задания частично выполнено

--- src/Main.java
+++ src/Main.java
@@ -4,56 +4,91 @@ public class Main {
 
         //Задание 1
 
-        int clientOsIOS = 0;
-        int clientOsAndroid = 1;
-        if (clientOsIOS <= 1) ;
-        if (clientOsAndroid <= 1) ;
-        System.out.println("Установите версию приложения для iOS по ссылке");
-        System.out.println("Установите версию приложения для Android по ссылке");
-
-        //Задание 2
-
-        int clientAndroid = 1;
-        int clientDeviceYea = 2012;
-        if (clientAndroid == 1 && clientDeviceYea <= 2014) {
-            System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (clientAndroid == 1 && clientDeviceYea >= 2015) {
-            System.out.println("Установите версию для Андроид по ссылке");
-        } else if (clientAndroid == 0 && clientDeviceYea <= 2014) {
-            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        int clientOs = 1;
+        if (clientOs == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            System.out.println("Установите версию для IOS");
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+            //Задание 2
+
+            int clientOS = 0;
+            int clientDeviceYear = 2020;
+            if (clientOS == 0) {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+            } else {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+
+            }
+
+            //Задание 3
+
+            int year = 2020;
+            if (year / 4 == 0 && year / 100 != 0 || year / 400 == 0) {
+                System.out.println(year + "является високосным");
+            } else {
+                System.out.println(year + "является не високосным");
+            }
         }
 
-        //Задание 3
+        //Задание 4
 
-        int year = 2021;
-        if (year / 4 == 0 && year / 100 != 8 || year / 408 == 0) {
-            System.out.println("Год" + year + "является високосным");
-        } else {
-            System.out.println("Год" + year + "является не високосным");
+        int deliveryDistance = 95;
+        int deliveryDays = 1;
+        if (deliveryDistance > 20) {
+            deliveryDays++;
+        }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Потребуется дней: " + deliveryDays);
+        {
+
         }
     }
 
-    //Задание 4
 
-    int deliveryDistance = 22;
-        if(deliveryDistance <=20)
+    //Задание 5
 
     {
-
-        System.out.println("Доставка займет сутки");
-    }
-        if(deliveryDistance >=20&&deliveryDistance <=100)
-
-    {
-        System.out.println();
+        int monthNumber = 5;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2:
+                System.out.println("Зима");
+                break;
+            case 3:
+            case 4:
+            case 5:
+                System.out.println("Весна");
+                break;
+            case 6:
+            case 7:
+            case 8:
+                System.out.println("Лето");
+                break;
+            case 9:
+            case 10:
+            case 11:
+                System.out.println("Осень");
+                break;
+            default:
+                System.out.println("Некорректный месяц:" + monthNumber);
+        }
     }
 }
 
 
 
-        //Задание 5
 
 
 
@@ -61,4 +96,6 @@ public class Main {
 
 
 
-    }
+
+
+
